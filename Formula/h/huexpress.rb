f9dce93f294eab1e77710ae3167ae7e28a74860f
Rui Chen <rui@chenrui.dev>
2024-09-13 19:18:42 -0400

    huexpress: add build patch for newer clang
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/huexpress.rb
+++ Formula/h/huexpress.rb
@@ -32,6 +32,10 @@ class Huexpress < Formula
     depends_on "mesa-glu"
   end
 
+  # Workaround for newer Clang
+  # upstream bug report, https://github.com/kallisti5/huexpress/issues/19
+  patch :DATA
+
   def install
     # Don't statically link to libzip.
     inreplace "src/SConscript", "pkg-config --cflags --libs --static libzip", "pkg-config --cflags --libs libzip"
@@ -43,3 +47,22 @@ class Huexpress < Formula
     assert_match(/Version #{version}$/, shell_output("#{bin}/huexpress -h", 1))
   end
 end
+
+__END__
+diff --git a/SConstruct b/SConstruct
+index 096a1ef..98216b9 100644
+--- a/SConstruct
++++ b/SConstruct
+@@ -40,5 +40,12 @@ env.Append(CPPDEFINES={'VERSION_MAJOR' : '3'})
+ env.Append(CPPDEFINES={'VERSION_MINOR' : '0'})
+ env.Append(CPPDEFINES={'VERSION_UPDATE' : '4'})
+ 
++# Workaround for newer Clang
++clang_version = env['CCVERSION']
++if env['PLATFORM'] == 'darwin' and 'clang' in env['CC']:
++    clang_version_parts = [int(part) for part in clang_version.split('.')]
++    if clang_version_parts >= [15, 0, 0]:
++        env.Append(CFLAGS=['-Wno-incompatible-function-pointer-types', '-Wno-int-conversion'])  # Pass as separate flags
++
+ Export("env")
+ SConscript('src/SConscript')
